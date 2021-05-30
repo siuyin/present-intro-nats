4292239fd6f8d65ec7f02861cc2370dd475f33d1
Loh Siu Yin <siuyin@beyondbroadcast.com>
2021-05-30 03:18:26 +0000

    refactor: move convenience functions into strm package

--- cmd/stream/main.go
+++ cmd/stream/main.go
@@ -1,19 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
-	"fmt"
-	"log"
 
 	"github.com/nats-io/nats.go"
-	"github.com/siuyin/dflt"
+	"github.com/siuyin/present-intro-nats/strm"
 )
 
-var (
-	nc         *nats.Conn
-	deleteMode bool
-)
+var deleteMode bool
 
 func init() {
 	flag.BoolVar(&deleteMode, "delete", false,
@@ -23,60 +17,14 @@ If this flag is not provided, it creates an example stream.`)
 }
 
 func main() {
-	js := jsCtx()
+	var nc *nats.Conn
+	js := strm.JetStreamContext(nc)
 	defer nc.Close()
 
 	const strName = "tst"
 	if !deleteMode {
-		createStream(js, strName)
+		strm.Create(js, strName)
 		return
 	}
-	deleteStream(js, strName)
-}
-
-func jsCtx() nats.JetStreamContext {
-	var err error
-	natsURL := dflt.EnvString("NATS_URL", "nats://127.0.0.1:4222")
-	nc, err = nats.Connect(natsURL)
-	if err != nil {
-		log.Fatalf("could not connect to NATS: %v", err)
-	}
-
-	js, err := nc.JetStream()
-	if err != nil {
-		log.Fatal("could not create JetStream context: %v", err)
-	}
-
-	return js
-}
-
-func createStream(js nats.JetStreamContext, name string) *nats.StreamInfo {
-	fmt.Printf("Creating stream: %q\n", name)
-	strInfo, err := js.AddStream(&nats.StreamConfig{
-		Name:     name,
-		Subjects: []string{"test.>", "test"},
-		MaxAge:   0, // 0 means keep forever
-		Storage:  nats.FileStorage,
-	})
-	if err != nil {
-		log.Panicf("could not create stream: %v", err)
-	}
-
-	prettyPrint(strInfo)
-	return strInfo
-}
-
-func prettyPrint(x interface{}) {
-	b, err := json.MarshalIndent(x, "", "  ")
-	if err != nil {
-		log.Fatalf("could not prettyPrint: %v", err)
-	}
-	fmt.Println(string(b))
-}
-
-func deleteStream(js nats.JetStreamContext, name string) {
-	fmt.Printf("Deleting stream: %q\n", name)
-	if err := js.DeleteStream(name); err != nil {
-		log.Printf("error deleting stream: %v", err)
-	}
+	strm.Delete(js, strName)
 }
---
+++ strm/strm.go
@@ -0,0 +1,61 @@
+// Package strm provides convienience functions to manipulate NATS JetsStreams.
+package strm
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/nats-io/nats.go"
+	"github.com/siuyin/dflt"
+)
+
+// JetStreamContext takes a nats connections and returns a JetStreamContext.
+func JetStreamContext(nc *nats.Conn) nats.JetStreamContext {
+	var err error
+	natsURL := dflt.EnvString("NATS_URL", "nats://127.0.0.1:4222")
+	nc, err = nats.Connect(natsURL)
+	if err != nil {
+		log.Fatalf("could not connect to NATS: %v", err)
+	}
+
+	js, err := nc.JetStream()
+	if err != nil {
+		log.Fatalf("could not create JetStream context: %v", err)
+	}
+
+	return js
+}
+
+// Create creates the named stream.
+func Create(js nats.JetStreamContext, name string) *nats.StreamInfo {
+	fmt.Printf("Creating stream: %q\n", name)
+	strInfo, err := js.AddStream(&nats.StreamConfig{
+		Name:     name,
+		Subjects: []string{"test.>", "test"},
+		MaxAge:   0, // 0 means keep forever
+		Storage:  nats.FileStorage,
+	})
+	if err != nil {
+		log.Panicf("could not create stream: %v", err)
+	}
+
+	prettyPrint(strInfo)
+	return strInfo
+}
+
+func prettyPrint(x interface{}) {
+	b, err := json.MarshalIndent(x, "", "  ")
+	if err != nil {
+		log.Fatalf("could not prettyPrint: %v", err)
+	}
+	fmt.Println(string(b))
+}
+
+// Delete deletes the named stream.
+func Delete(js nats.JetStreamContext, name string) {
+	fmt.Printf("Deleting stream: %q\n", name)
+	if err := js.DeleteStream(name); err != nil {
+		log.Printf("error deleting stream: %v", err)
+	}
+}
